e4cdff23a17f8db6e3f664bf7cbdf23d006a63be
poonambhattad <poonam.bhattad@thoughtworks.com>
2020-07-12 15:14:21 +0530

    Introduced state property

--- firstapp/src/components/Header.js
+++ firstapp/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Header(){
     return(
-        <div>I am the header of the application</div>
+        <div className="navbar">I am the header of the application</div>
     )
 }
 
