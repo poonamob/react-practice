8eff261082e1338c8dbd5f44ffbfa3dd3311fc11
poonambhattad <poonam.bhattad@thoughtworks.com>
2020-07-13 20:00:22 +0530

    Fully functional vidly app

---
+++ vidly/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Movies from "./component/movie";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
